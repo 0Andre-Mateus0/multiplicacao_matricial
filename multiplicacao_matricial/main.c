dce333f68cb13ba3459795f232881c84512d142b
André Mateus <andrem.03p@gmail.com>
2025-09-03 22:54:15 -0300

    Base

---
+++ multiplicacao_matricial/main.c
@@ -0,0 +1,35 @@
+//Headers
+#include <stdio.h>
+#include <stdlib.h>
+
+//MACROS (constantes)
+
+//Funções auxiliares
+
+//Main
+void main() {
+    printf("Hello world!\n");
+
+}
+
+/*
+                    Instruções gerais
+                    por: André Mateus
+
+  1. Tela onde é pedida do usuário as ordens das
+     matrizes.
+
+  2. Caso a condição de multiplicação matricial
+     não for atendida: 2.1; caso contrário: 3;
+
+    2.1. Tela avisando ao usuário que dá erro e
+         possui duas opções: o usuário pôr outros
+         números como ordem ou sair.
+
+  3. Pergunta se o usuário se ele quer esoolher
+     manualmente os números ou pseudoaleatório.
+
+  4. Finalizada a multiplicação, pergunta se o usuário
+     quer refazer com outros números ou sair.
+
+*/
