d204cce18d79c5f453bfcf16d6692f98837f5047
André Mateus <andrem.03p@gmail.com>
2025-09-05 02:02:05 -0300

    desenharGUI
    Criação de uma função padrão que permite desenhar no console e possui parâmetros opcionais

--- multiplicacao_matricial/main.c
+++ multiplicacao_matricial/main.c
@@ -2,25 +2,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <conio.h>
+
+/*
+05/09(AM): Essa foi a melhor maneira que eu consegui para criar parâmetros opcionais. É mais legível e possui uma fácil manutenção.
+*/
+
+//Estrutura para configuração do desenho da GUI
+typedef struct {
+    int espacY1;
+    int espacX1;
+    int espacY2;
+    int espacX2;
+} configGUI;
 
 //MACROS (constantes)
+/*
+    05/09 (AM): Tive que criar esse macro pois pGUI é um ponteiro e se eu quisesse calcular internamente o tamanho do array eu teria que usar NULL para marcar o fim do array. E eu não tô com saco pra isso.
+*/
+//Macro para calcular tamanho.
+#define TAM_ARRAY(arr) (sizeof(arr)/sizeof((arr)[0]))
+//Valor padrão para configGUI
+#define CONFIG_PADRAO {0,0,0,0}
+
 
 //Funções prototipos
-void telaTitulo();
-
-//Main
-void main() {
-    setlocale(LC_ALL,"");
-    telaTitulo();
-}
+//05/09 (AM): É obrigatório escrever aqui as funções auxliares, se não dá erro de declaração implícita.
+void desenharGUI();
 
 
 //Funções auxiliares
 
 //Cria uma tela de título, óbvio (AM)
 //04/09 (AM): Eu vou tentar transformar essa função numa recursão.
-void telaTitulo() {
-    char *titulo[] = {
+/*
+05/09 (AM): Não dá.
+      (AM): Eu vou tentar tornar essa função em uma função geral com parâmetros opcionais.
+      (AM): Eba!!
+*/
+void desenharGUI(const char *pGUI[], int tamArray, configGUI config) {
+
+    for(int i = 0; i < config.espacY1; i++) { printf("\n"); }
+    for(int i = 0; i < tamArray; i++) {
+        for(int j = 0; j < config.espacX1; j++) { printf(" "); }
+        printf("%s\n",pGUI[i]);
+    }
+    for(int i = 0; i < config.espacY2; i++) { printf("\n"); }
+    for(int i = 0; i < config.espacX2; i++) { printf(" "); }
+}
+
+
+
+//Main
+void main() {
+    setlocale(LC_ALL,"");
+
+    const char *titulo[] = {
         "___  ___      _ _   _       _ _                            ___  ___      _        _      _       _ ",
         "|  \\/  |     | | | (_)     | (_)                           |  \\/  |     | |      (_)    (_)     | |",
         "| .  . |_   _| | |_ _ _ __ | |_  ___ __ _  ___ __ _  ___   | .  . | __ _| |_ _ __ _  ___ _  __ _| |",
@@ -30,17 +67,15 @@ void telaTitulo() {
         "                     | |                                                                           ",
         "                     |_|                                                                           "
     };
-    int tamArray = sizeof(titulo)/sizeof(titulo[0]);
 
-    for(int i = 0; i < 10; i++) { printf("\n"); }
-    for(int i = 0; i < tamArray; i++) {
-        for(int j = 0; j < 10; j++) { printf(" "); }
-        printf("%s\n",titulo[i]);
-    }
-    for(int i = 0; i < 3; i++) { printf("\n"); }
-    for(int i = 0; i < 50; i++) { printf(" "); }
+    desenharGUI(titulo,TAM_ARRAY(titulo),(configGUI){.espacY1 = 10, .espacX1 =10, .espacY2 = 3, .espacX2 = 50});
     printf("<ENTER> para executar");
+    getch();
+    system("cls");
 }
+
+
+
 
 /*
                     Instruções gerais
