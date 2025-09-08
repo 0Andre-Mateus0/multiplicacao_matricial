d12dac55b7406187f394d25081e3c47ab4f25d39
André Mateus <andrem.03p@gmail.com>
2025-09-08 02:06:14 -0300

    GUI tela de ordenadas
    Criada e arrumada a GUI da tela de ordenadas.

--- multiplicacao_matricial/main.c
+++ multiplicacao_matricial/main.c
@@ -10,10 +10,10 @@
 
 //Estrutura para configuração do desenho da GUI
 typedef struct {
-    int espacY1;
-    int espacX1;
-    int espacY2;
-    int espacX2;
+    int espacY1; //Define o quâo para baixo começará o texto.
+    int espacX1; //Define o quâo à direita está o texto.
+    int espacY2; //Define o quâo para baixo estará o que quer que seja.
+    int espacX2; //Define o quâo à direita estará o que quer que seja.
 } configGUI;
 
 //MACROS (constantes)
@@ -22,14 +22,16 @@ typedef struct {
 */
 //Macro para calcular tamanho.
 #define TAM_ARRAY(arr) (sizeof(arr)/sizeof((arr)[0]))
-//Valor padrão para configGUI
+//Valor padrão para configGUI (Nunca será usado :3 )
 #define CONFIG_PADRAO {0,0,0,0}
 
-
+/*
 //Funções prototipos
 //05/09 (AM): É obrigatório escrever aqui as funções auxliares, se não dá erro de declaração implícita.
+  05/09 (AM): Aparentemente eu estava enganado. Ops...
 void desenharGUI();
-
+int ordemMatrizes();
+*/
 
 //Funções auxiliares
 
@@ -41,7 +43,6 @@ void desenharGUI();
       (AM): Eba!!
 */
 void desenharGUI(const char *pGUI[], int tamArray, configGUI config) {
-
     for(int i = 0; i < config.espacY1; i++) { printf("\n"); }
     for(int i = 0; i < tamArray; i++) {
         for(int j = 0; j < config.espacX1; j++) { printf(" "); }
@@ -51,13 +52,50 @@ void desenharGUI(const char *pGUI[], int tamArray, configGUI config) {
     for(int i = 0; i < config.espacX2; i++) { printf(" "); }
 }
 
+//Pra fazer respostaEntreTextos funcionar :3
+void gotoxy(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+}
+
+//Digitar resposta entre textos
+void respostaEntreTextos(int coluna, int linha,int *resposta) {
+    gotoxy(coluna,linha);
+    scanf("%d",resposta);
+}
+
+//Pede as ordens das matrizes e, se forem compatíveis parte para a função que pede os números.
+int ordemMatrizes() {
+    int matrizAOrd1, matrizAOrd2, matrizBOrd1, matrizBOrd2;
+
+    const char *instrucoesOrdens[] = {
+        "Por favor, digite as ordens das matrizes:",
+    }, *GUIOrdens[] = {
+        "  ___                  ______ ",
+        " / _ \\      __  __     | ___ \\",
+        "/ /_\\ \\     \\ \\/ /     | |_/ /",
+        "|  _  |      >  <      | ___ \\",
+        "| | | |     /_/\\_\\     | |_/ /",
+        "\\_| |_/  x             \\____/  x  "
+    };
+
+    desenharGUI(instrucoesOrdens,TAM_ARRAY(instrucoesOrdens),(configGUI){.espacY1 = 10, .espacX1 = 40, .espacY2 = 3});
+    desenharGUI(GUIOrdens,TAM_ARRAY(GUIOrdens),(configGUI){.espacX1 = 45, .espacY2 = 3});
+    respostaEntreTextos(53,20,&matrizAOrd1);
+    respostaEntreTextos(56,20,&matrizAOrd2);
+    respostaEntreTextos(75,20,&matrizBOrd1);
+    respostaEntreTextos(78,20,&matrizBOrd2);
+
+    printf("\n");
+    for(int i = 0; i < 50; i++) printf(" ");
+}
 
 
 //Main
 void main() {
     setlocale(LC_ALL,"");
 
-    const char *titulo[] = {
+    //Variáveis
+    const char *telaInicial[] = {
         "___  ___      _ _   _       _ _                            ___  ___      _        _      _       _ ",
         "|  \\/  |     | | | (_)     | (_)                           |  \\/  |     | |      (_)    (_)     | |",
         "| .  . |_   _| | |_ _ _ __ | |_  ___ __ _  ___ __ _  ___   | .  . | __ _| |_ _ __ _  ___ _  __ _| |",
@@ -68,8 +106,14 @@ void main() {
         "                     |_|                                                                           "
     };
 
-    desenharGUI(titulo,TAM_ARRAY(titulo),(configGUI){.espacY1 = 10, .espacX1 =10, .espacY2 = 3, .espacX2 = 50});
+    //Desenha a tela de título
+    desenharGUI(telaInicial,TAM_ARRAY(telaInicial),(configGUI){.espacY1 = 10, .espacX1 = 10, .espacY2 = 3, .espacX2 = 50});
     printf("<ENTER> para executar");
+    getch();
+    system("cls");
+
+    ordemMatrizes();
+    printf("<ENTER> para continuar");
     getch();
     system("cls");
 }
